58991b39bff57fed70436d6ec5d2b7df0a8bdc03
Ashish rana <ashishrana288@gmail.com>
2025-06-09 13:49:33 +0530

    Update practice.php

--- practice.php
+++ practice.php
@@ -682,6 +682,24 @@ function narcissistic(int $number){
     }
   }
 
+	//Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+//example - 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit, there are 3 multiplications)
+
+function persistence(int $num): int {
+    $sum = 0;
+    while($num >= 10){
+        $digits = str_split($num);
+        $mul = 1;
+        foreach($digits as $digit){
+            $mul *= $digit;
+        }
+        $num = $mul;
+        $sum++;
+    }
+      return $sum;
+    }
+
+
 }
 
 
