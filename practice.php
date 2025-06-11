c762282e52863b0936e83a002eae2d86e6f9c0bc
Ashish rana <ashishrana288@gmail.com>
2025-06-11 09:32:00 +0530

    Update practice.php

--- practice.php
+++ practice.php
@@ -694,6 +694,20 @@ function persistence(int $num): int {
       return $sum;
     }
 
+function is_prime(int $n): bool {
+    if ($n <= 1) {
+        return false;
+    }
+
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($n % $i == 0) {
+            return false; // Not prime
+        }
+    }
+
+    return true; // Prime
+}
+
 
 }
 
